23a381a57b7c9041f7748941ccd5dfab12105f8c
amila96 <64005937+amila96@users.noreply.github.com>
2021-06-25 17:43:46 +0530

    Contact Us

--- src/app/component/dash-board/dash-board.component.ts
+++ src/app/component/dash-board/dash-board.component.ts
@@ -1,64 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {CustomerService} from "../../service/customer.service";
-import CustomerDTO from "../../dto/CustomerDTO";
-import {ContactUsMessageService} from "../../service/contact-us-message.service";
+import {ContactUsMessageService} from '../../service/contact-us-message.service';
 import ContactUsMessageDTO from '../../dto/ContactUsMessageDTO';
+import { animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
-  styleUrls: ['./dash-board.component.scss']
+  styleUrls: ['./dash-board.component.scss'],
+  animations:[
+    trigger( 'sliderLeft',[
+      transition('void=>*',[
+        style( {transform:'translateX(-100%)'}),
+        animate('0.5s')
+      ]),
+      transition('*=>void',[
+        animate( '0.5s',style({ transform:'translateX(-100%)'}))
+      ])
+    ])
+  ]
 })
-
-
 
 export class DashBoardComponent implements OnInit {
 
-  constructor(private customerService: CustomerService, private contactUsMessageService: ContactUsMessageService) {
+  constructor(private contactUsMessageService: ContactUsMessageService) {
   }
 
-  customerName = '';
-  customerAddress = '';
-  customerSalary = 0;
-  pickupLocation='';
-  distance=0;
-  customerNameForUpdate = '';
-  customerAddressForUpdate = '';
-  customerSalaryForUpdate = 0;
-
-
-
-  customerList: any[] = [];
   contactUsMessageList: any[] = [];
   airportCustomerName = '';
   airportCustomerEmail = '';
   airportCustomerSubject = '';
   airportCustomerMessage = '';
 
-  selectedCustomer: any = null;
+  /*selectedCustomer: any = null;*/
 
   ngOnInit(): void {
-    this.loadAllCustomers();
+
     this.loadAllMessages();
   }
 
-  loadAllCustomers() {
-    this.customerService.getAllCustomers().subscribe(response => {
-      this.customerList = response.dataSet;
-    }, error => {
-      console.log(error);
-    });
+  leftSideVisibleState= false;
+
+  toggleSlide(){
+    this.leftSideVisibleState=!this.leftSideVisibleState;
   }
-  deleteCustomer(_id: string) {
-    if (confirm('Are You sure?')) {
-      this.customerService.deleteCustomer(_id).subscribe(respose => {
-        this.loadAllCustomers();
-        alert('Deleted!');
-      }, error => {
-        console.log(error);
-      });
-    }
-  }
+
   loadAllMessages() {
     this.contactUsMessageService.getAllMessages().subscribe(response => {
       this.contactUsMessageList = response.dataSet;
@@ -66,6 +51,7 @@ export class DashBoardComponent implements OnInit {
       console.log(error);
     });
   }
+
   deleteMessage(_id: string) {
     if (confirm('Are You sure?')) {
       this.contactUsMessageService.deleteMessage(_id).subscribe(respose => {
@@ -76,48 +62,4 @@ export class DashBoardComponent implements OnInit {
       });
     }
   }
-
-
- /* saveCustomer() {
-
-    const dto = new CustomerDTO(
-      this.customerName.trim(),
-      this.customerAddress.trim(),
-      Number(this.customerSalary),
-      'no-image',
-      []
-    );
-
-    this.customerService.saveCustomer(dto).subscribe(resp => {
-      alert(resp.message);
-    }, error => {
-      console.log(error);
-    });
-  }*/
-
-
-
-  /*openModel(customer: any) {
-    this.selectedCustomer = customer;
-    /!* const btn = document.getElementById('updatebutton') as HTMLElement;
-     btn.click();*!/
-    document.getElementById('updatebutton').click();
-  }*/
-
- /* updateCustomer() {
-    const dto = new CustomerDTO(
-      this.customerNameForUpdate.trim(),
-      this.customerAddressForUpdate.trim(),
-      Number(this.customerSalaryForUpdate),
-      '',
-      [],
-    );
-    this.customerService.updateCustomer(dto, this.selectedCustomer._id).subscribe(response => {
-      alert('Updated');
-    }, error => {
-      console.log(error);
-    });
-  }*/
-
-
 }
